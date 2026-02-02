6a6bf660cf75fc18509a9774ceead2f42a6786a6
pradhumn panchal <pradhumn44@gmail.com>
2026-02-02 14:32:06 +0530

    Update deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === "production" ? "/notion-flow-portfolio/" : "/",
+  base: mode === "production" ? "/notion-flow-portfolio" : "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
